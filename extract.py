2563b598e3ddf9e421ad09508f345bc5b69461df
ParthPatel2000 <arjun.823821@gmail.com>
2021-05-04 15:10:26 +0530

    Extraction is Completely working now. Changed output format to png for now JPG was problematic.

--- extract.py
+++ extract.py
@@ -1,36 +1,60 @@
 from PIL import Image
 import numpy as np
 
-photo = Image.open("stg.jpg")
+def tex2bin(secret):
+    bits = ''.join(format(i, '08b') for i in bytearray(secret, encoding='utf-8'))
+    return bits
+
+def bin2txt(string):
+    text = ""
+    while string:
+        byte = string[:8]
+        ascii = int(byte, 2)
+        text += chr(ascii)
+        string = string[8:]
+    return text
+
+def Length_extract(data):
+    length = ''
+    for i in range(width):
+        x = 0
+        length += bin(data[x][i][0])[-1]
+        length += bin(data[x][i][1])[-1]
+        length += bin(data[x][i][2])[-1]
+    length = bin2txt(length).split('<l>')[1]
+    length = (int(length)-16)*4
+    # print(length)
+    return length
+
+
+
+
+photo = Image.open("stg.PNG")
 data = np.asarray(photo)
 width, height = photo.size
+# print(data[0])
 
 secret = ''
+secretlength = Length_extract(data)
+# print(data)
 
-print(data)
-# count = 0
-# for x in range(height):
-#     for y in range(width):
-#             red = bin(data[x][y][0])[2:]
-#
-#             # print(red[len(red)-1],end='')
-#             secret += red[len(red) - 1]
-#             count +=1
-#             if count == 5328:
-#                 break
-#             green = bin(data[x][y][1])[2:]
-#             secret += green[len(green) - 1]
-#             count += 1
-#             if count == 5328:
-#                 break
-#             blue = bin(data[x][y][2])[2:]
-#             # print(green[len(green)-1],end='')
-#             secret += blue[len(blue) - 1]
-#             # print(blue[len(blue) - 1],end='')
-#             count += 1
-#             if count == 5328:
-#                 break
-#
-#
-# print(secret)
-# print(count)
+count = 0
+for x in range(1,height):
+    for y in range(width):
+
+        if count < secretlength:
+            secret += bin(data[x][y][0])[-1]
+            count+=1
+            if count == secretlength:
+                break
+            secret += bin(data[x][y][1])[-1]
+            count += 1
+            if count == secretlength:
+                break
+            secret += bin(data[x][y][2])[-1]
+            count += 1
+            if count == secretlength:
+                break
+
+print("secret -> ", bin2txt(secret))
+print(count)
